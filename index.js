f3c35c6c1888576e4b6c9f9717b366420b6311e4
taingchhayse <62208941+taingchhayse@users.noreply.github.com>
2022-09-13 22:29:49 +0700

    add express

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-require("dotenv").config();
-
+require('dotenv').config()
+const express = require('express')
 /* Import our client structure */
-const Bot = require("./src/struct/Bot");
-const client = new Bot();
+const Bot = require('./src/struct/Bot')
+const client = new Bot()
+const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get('/', (req, res) => {
+  console.log('Getting a [GET] request')
+  res.send({ status: 'sccuess', msg: 'you are great' })
+})
+app.listen(8080)
+console.log('Now listen to port 8080')
 /* Call our start function to load the bot instance */
-(async () => await client.start(process.env.TOKEN))();
+;(async () => await client.start(process.env.TOKEN))()
